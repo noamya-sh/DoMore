9e8b49cf0195dbec486c66b864d2015cc44807fe
noamya-sh <noamyashany@gmail.com>
2022-12-08 23:02:24 +0200

    listview

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         temp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, second_activity.class));
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
             }
         });
 
--- app/src/main/java/com/example/myapplication/listAdapterVolunteering.java
+++ app/src/main/java/com/example/myapplication/listAdapterVolunteering.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,12 +11,17 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class listAdapterVolunteering extends ArrayAdapter<volunteering> {
+    private Context mContext;
+    private int mResource;
 
-    public listAdapterVolunteering(Context context, ArrayList<volunteering> volArrayList) {
-        super(context,R.layout.list_view ,volArrayList);
+    public listAdapterVolunteering(Context context,int resource,ArrayList<volunteering> volArrayList) {
+        super(context,resource ,volArrayList);
+        this.mContext = context;
+        this.mResource = resource;
     }
 
     @NonNull
@@ -24,7 +30,7 @@ public class listAdapterVolunteering extends ArrayAdapter<volunteering> {
         volunteering v = getItem(position);
 
         if (convertView == null){
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_view,parent,false);
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.list_view,parent,false);
         }
 
         TextView name = convertView.findViewById(R.id.associationName);
@@ -33,14 +39,14 @@ public class listAdapterVolunteering extends ArrayAdapter<volunteering> {
 //        TextView startdate = convertView.findViewById(R.id.date);
         TextView startTime = convertView.findViewById(R.id.strtTime);
         TextView endTime = convertView.findViewById(R.id.endTime);
-
+        SimpleDateFormat dt1 = new SimpleDateFormat("hh:mm");
         name.setText(v.association);
         title.setText(v.title);
         city.setText(v.location);
-        startTime.setText(v.start_date.toString());
-        endTime.setText(v.end_date.toString());
+        startTime.setText(dt1.format(v.start_date));
+        endTime.setText(dt1.format(v.end_date));
 
-        return super.getView(position, convertView, parent);
+        return convertView;//super.getView(position, convertView, parent);
     }
 
 }
