9ac9c160b0b0cb120e438d9482d3d3556beb26e0
noamya-sh <noamyashany@gmail.com>
2022-12-11 21:43:45 +0200

    add datePicker and timePicker

--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -1,12 +1,20 @@
 package com.example.myapplication;
 
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -48,6 +56,57 @@ public class SecondActivity extends AppCompatActivity {
                 String s = cat.getSelectedItem().toString();
                 ArrayList<volunteering> new_volList = new ArrayList<>();
 
+            }
+        });
+        Button calendarButton = findViewById(R.id.calendar_button);
+        calendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open the calendar here
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int hour = calendar.get(java.util.Calendar.HOUR);
+                int minute = calendar.get(java.util.Calendar.MINUTE);
+                DatePicker datePicker = new DatePicker(v.getContext());
+                datePicker.init(year, month, day, null);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setView(datePicker);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Get the selected date from the date picker
+                        int year = datePicker.getYear();
+                        int month = datePicker.getMonth();
+                        int day = datePicker.getDayOfMonth();
+
+                        // Create a time picker and set the selected hour and minute
+                        TimePicker timePicker = new TimePicker(v.getContext());
+                        timePicker.setHour(hour);
+                        timePicker.setMinute(minute);
+
+                        AlertDialog.Builder timePickerBuilder = new AlertDialog.Builder(v.getContext());
+                        timePickerBuilder.setView(timePicker);
+                        timePickerBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Get the selected time from the time picker
+                                int hour = timePicker.getHour();
+                                int minute = timePicker.getMinute();
+
+                                // Do something with the selected date and time
+                            }
+                        });
+
+                        // Show the time picker dialog
+                        timePickerBuilder.show();
+                    }
+                });
+
+                // Show the date picker dialog
+                builder.show();
             }
         });
     }
