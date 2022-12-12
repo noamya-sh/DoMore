c41ed2c826f45ac37dddd87fca5de2d847f97ed5
noamya-sh <noamyashany@gmail.com>
2022-12-12 19:12:32 +0200

    add search dialog

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         temp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                startActivity(new Intent(MainActivity.this, VolunteeringListActivity.class));
             }
         });
 
--- app/src/main/java/com/example/myapplication/VolunteeringListActivity.java
+++ app/src/main/java/com/example/myapplication/VolunteeringListActivity.java
@@ -2,29 +2,15 @@ package com.example.myapplication;
 
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
-import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
-import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.TimePicker;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -34,19 +20,18 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Map;
 
-public class SecondActivity extends AppCompatActivity implements DialogListener {
+public class VolunteeringListActivity extends AppCompatActivity implements DialogListener {
     public ArrayList<volunteering> volList = new ArrayList<>();
 
     private ListView listView;
     private VolunteeringAdapter adapter;
-    private Button search;
-    AlertDialog dialog;
+    AlertDialog loadingDialog;
     FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_volunteering_list);
         listView = findViewById(R.id.shapesListView);
         adapter = new VolunteeringAdapter(this, volList);
         listView.setAdapter(adapter);
@@ -56,8 +41,8 @@ public class SecondActivity extends AppCompatActivity implements DialogListener 
         View dialogView = getLayoutInflater().inflate(R.layout.dialog_progress, null);
         builder.setView(dialogView);
         builder.setCancelable(false);
-        dialog = builder.create();
-        dialog.show();
+        loadingDialog = builder.create();
+        loadingDialog.show();
 
         setupData();
         listView.setOnItemClickListener((parent, view, position, id) -> System.out.println("yess"));
@@ -77,7 +62,7 @@ public class SecondActivity extends AppCompatActivity implements DialogListener 
                 for (QueryDocumentSnapshot document : task.getResult()) {
                     if (document.exists()){
                         init_vol(document);
-                        dialog.dismiss();
+                        loadingDialog.dismiss();
                     }
                 }
             } else {
@@ -88,9 +73,9 @@ public class SecondActivity extends AppCompatActivity implements DialogListener 
     public void init_vol(QueryDocumentSnapshot document){
         DocumentReference f = document.getDocumentReference("association");
         DocumentReference dd = db.document(f.getPath());
-        dd.get().addOnCompleteListener(task1 -> {
-            if (task1.isSuccessful()){
-                DocumentSnapshot document2 = task1.getResult();
+        dd.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                DocumentSnapshot document2 = task.getResult();
                 String association = document2.getString("name");
                 volunteering v = new volunteering(
                         association,
